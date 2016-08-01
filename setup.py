b3eaebd08b306755badd784bfa5b3db96c586f87
Jonathan Terhorst <terhorst@gmail.com>
2016-08-01 15:49:42 -0700

    add setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+        name = "stepfun",
+        version = "0.0.1",
+        author = "Jonathan Terhorst",
+        author_email = "terhorst@gmail.com",
+        description = "A class for representing step functions",
+        license = "GPL",
+        keywords = "step function",
+        packages = ['stepfun'],
+        install_requires=['numpy']
+        )
